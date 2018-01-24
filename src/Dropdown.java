a94779a4df85722345df35003f1d196d35e7006c
Allan <allan.carlo.t.ramos@gmail.com>
2018-01-25 06:43:55 +0800

    adding dropdown

--- src/Dropdown.java
+++ src/Dropdown.java
@@ -16,10 +16,25 @@ public class Dropdown extends openDriver {
 	public void test1()
 	{
 		driver.get("http://spicejet.com");
+		//Static Dropdowns
 		Select s = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
 		s.selectByValue("2");
 		s.selectByIndex(6);
 		s.selectByVisibleText("4");
+		
+		//Dynamic Dropdowns
+		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='GOI']")).click();
+		//driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_destinationStation1_CTXT")).click();
+		driver.findElement(By.xpath("(//a[@value='DEL'])[2]")).click();
+		
+		//CHeckbox
+		if(driver.findElement(By.id("ctl00_mainContent_chk_Unmr")).isSelected() == false)
+		{
+			driver.findElement(By.id("ctl00_mainContent_chk_Unmr")).click();
+		}
+		//isSelected (want to know if checkbox is selected)
+		
 		
 	}
 }
