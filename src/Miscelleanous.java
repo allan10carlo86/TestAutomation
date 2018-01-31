a27f729562a7bc270cff57ce4ebe8441c903d8e1
allan <allan.carlo.t.ramos@gmail.com>
2018-01-31 14:23:54 +0800

    Misceleanous.java is committed to Master.

--- src/Miscelleanous.java
+++ src/Miscelleanous.java
@@ -2,6 +2,9 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Miscelleanous extends openDriver{
 	
@@ -11,17 +14,42 @@ public class Miscelleanous extends openDriver{
 		housekeeping();
 	}
 	@Test
-	public void test1()
+	public void test1() throws InterruptedException
 	{
 		driver.get("https://fantasycricket.dream11.com/in/");
-		driver.findElement(By.id("m_rtxtEmail1")).sendKeys("allan.carlo.t.ramos@gmail.com");
+		driver.findElement(By.id("m_rtxtEmail1")).sendKeys("allan.ca4rlo.t.ramos@gmail.com");
 		driver.findElement(By.id("m_rtxtPassword")).sendKeys("H3110W0r1d");
+		
+		
+		Select oDay = new Select(driver.findElement(By.id("m_selDate")));
+		oDay.selectByValue("1");
+		Select oMonth = new Select (driver.findElement(By.id("m_selMonth")));
+		oMonth.selectByVisibleText("Jan");
+		Select oYear = new Select(driver.findElement(By.id("m_selYear")));
+		oYear.selectByVisibleText("1994");
+		
 		boolean isSelected = driver.findElement(By.id("m_chkTerms")).isSelected();
 		System.out.println("Agreement Checkbox selected? " + isSelected);
+		
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		
+		
 		if(!isSelected)
-			driver.findElement(By.id("m_chkTerms")).click();
+		{	
+			Thread.sleep(3000);
+			WebElement ele = driver.findElement(By.name("m_chkTerms"));
+			executor.executeScript("document.getElementById(\"m_chkTerms\").checked=true;", ele);
+					
+		}
 		isSelected = driver.findElement(By.id("m_chkTerms")).isSelected();
+		System.out.println("Agreement Checkbox selected? " + isSelected);
+		
 		driver.findElement(By.id("registerClick")).click();	
+		Thread.sleep(5000);
+		
+		driver.findElement(By.cssSelector("a.redBtn.close")).click();
+		
+		
 	}
 	
 
