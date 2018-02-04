45ac51634c8a03c88278c37e68ca40b3b434e256
allancarloramos <allan.carlo.t.ramos@gmail.com>
2018-02-04 15:21:25 +0800

    Commit New Files - including AutoSuggest.java

--- src/AutoSuggest.java
+++ src/AutoSuggest.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
+import java.io.File;
+import java.io.IOException; 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
 /* 
  * Using Webdriverwait and Actions class 
  */
@@ -18,21 +22,58 @@ public class AutoSuggest extends openDriver {
 	@Test
 	public void test1()
 	{
-		driver.get("https://fantasycricket.dream11.com/IN/");
+		try {
+			driver.get("https://fantasycricket.dream11.com/IN/");
+			
+			WebElement emailIdElem = driver.findElement(By.xpath("//input[@id='m_rtxtEmail1']"));
+			emailIdElem.click();
+			emailIdElem.sendKeys("A");
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mailtip")));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(driver.findElement(By.xpath("//p[contains(text(),'A@yahoo.com')]"))).click().build().perform();
+			//builder.moveToElement(driver.findElement(By.xpath("//p[contains(text(),'A@yaho22o.com')]"))).click().build().perform();
+			
+			
+			//clear and ask for new path
+			emailIdElem.clear();
+			emailIdElem.sendKeys("B");
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mailtip")));
+			builder.sendKeys(Keys.DOWN).click().build().perform();
+
+			
+		} catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			//take screenshot
+			TakesScreenshot scrShot =((TakesScreenshot)driver); 
+			File src =  scrShot.getScreenshotAs(OutputType.FILE);
+			try {
+				
+				FileUtils.copyFile(src, new File("Screenshot/Error/error-screenshot.png"));
+			}
+			catch(IOException e1)
+			{
+				System.out.println(e1.getMessage());
+			}
+		}
+		finally
+		{
+			//take one more screenshtots
+			//take screenshot
+			TakesScreenshot scrShot1 =((TakesScreenshot)driver); 
+			File src1 =  scrShot1.getScreenshotAs(OutputType.FILE);
+			try {
+				
+				FileUtils.copyFile(src1, new File("Screenshot/Finally/finally-screenshot.png"));
+			}
+			catch(IOException e2)
+			{
+				System.out.println(e2.getMessage());
+			}
+			driver.quit();
+		}
 		
-		WebElement emailIdElem = driver.findElement(By.xpath("//input[@id='m_rtxtEmail1']"));
-		emailIdElem.click();
-		emailIdElem.sendKeys("A");
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mailtip")));
-		Actions builder = new Actions(driver);
-		builder.moveToElement(driver.findElement(By.xpath("//p[contains(text(),'A@yahoo.com')]"))).click().build().perform();
-		
-		//clear and ask for new path
-		emailIdElem.clear();
-		emailIdElem.sendKeys("B");
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mailtip")));
-		builder.sendKeys(Keys.DOWN).click().build().perform();
-		
+	
 		
 	}
 }
